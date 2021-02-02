b50f190326ea4bd1bd9b2bf53af7a3d5d91f9f4f
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2021-02-02 11:49:51 +0100

    Add carousel for desktop version, styled components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,18 @@
-import { Reset } from 'styled-reset'
+import { Reset } from "styled-reset";
 import { AppDiv } from "./styled-components/App";
-import Navbar from './components/Navbar'
-import MainPage from './components/Mainpage';
-import PresentationPage from './components/PresentationPage';
+import Navbar from "./components/Navbar";
+import MainPage from "./components/Mainpage";
+import PresentationPage from "./components/PresentationPage";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <AppDiv>
-      <Reset/>
-      <Navbar/>
-      <MainPage/>
-      <PresentationPage/>
+      <Reset />
+      <Navbar />
+      <MainPage />
+      <PresentationPage />
+      <Carousel />
     </AppDiv>
   );
 }
